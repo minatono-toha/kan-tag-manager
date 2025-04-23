cea0d4faef57a93488f7b92ea9443b74569f59b7
minatono-toha <toha_on_harbor@outlook.jp>
2025-04-24 01:58:36 +0900

    rename dist to docs

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/kan-tag-manager', // GitHub Pages 用
+  base: '/kan-tag-manager/', // GitHub Pages 用
 })
